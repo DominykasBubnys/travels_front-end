079eefd15bc6831b7099c2c0163767bbbec4ae67
dominykas <bubnys.it@gmail.com>
2022-08-22 13:33:06 +0300

    working with adding newComment functionality

--- src/places/components/PlaceCommentsList.js
+++ src/places/components/PlaceCommentsList.js
@@ -1,26 +1,48 @@
 import React, { useContext, useEffect, useState } from 'react'
 import LoadingSpinner from '../../shared/UIElements/LoadingSpinner'
-// import { AddNewComment } from '../../shared/utils/AddNewComment'
 import { AuthContext } from '../../shared/context/auth-context'
 import Button from '../../shared/FormElements/Button'
 import { useHistory } from 'react-router-dom'
-
-import './PlaceCommentsList.css'
 import PlaceComentItem from './PlaceComentItem'
+import { AddNewComment } from '../../shared/utils/AddNewComment'
+import './PlaceCommentsList.css'
+
 
 const CommentsList = (props) => {
 
   const [isloading, setIsLoading] = useState(false)
   const [commentsData, setCommentsData] = useState(null);
   const [commentsCount, setCommentsCount] = useState(0);
+  const [newCommentInput, setNewCommentInput] = useState("");
+  const [isButtonDisable, setIsButtonDisable] = useState(true);
+  const [refreshCommentsData, setRefreshCommentsData] = useState(false);
   const Auth = useContext(AuthContext);
   const history = useHistory()
 
-  const addNewCommentHandler = () => {
-    console.log("adding new comment...")
+  const addNewCommentHandler = async() => {
+    setRefreshCommentsData(true)
+
+
+    try {
+      const request = await AddNewComment(props.pid, Auth.userId, newCommentInput)
+
+      if(!request.status)throw new Error("Cannot send a comment");
+    } catch (error) {
+
+      console.log("failed to add a new comment");
+    }
+  }
+
+  const newCommentInputHandler = (event) => {
+    const input = event.target.value;
+    setIsButtonDisable(true);
+    if(input.length > 0)setIsButtonDisable(false);
+    setNewCommentInput(event.target.value || "")
   }
 
   useEffect(()=>{
+    console.log("useefect");
+    
     const loadPlaceComments = async() => {
       try {
         setIsLoading(true);
@@ -36,6 +58,7 @@ const CommentsList = (props) => {
         setCommentsData(reqData.comments)
         setCommentsCount(reqData.count)
         setIsLoading(false);
+        setRefreshCommentsData(false);
       if(!Req.ok){
         throw new Error("failed to post new place");
       }
@@ -48,7 +71,7 @@ const CommentsList = (props) => {
     }
 
     loadPlaceComments();
-  }, [])
+  }, [refreshCommentsData])
 
 
   return (
@@ -60,9 +83,10 @@ const CommentsList = (props) => {
         
         <div>
           {  Auth.isLoggedIn ? 
-            <button className='new-comment-btn' onClick={addNewCommentHandler}>
-              Add new comment...
-            </button>
+            <div className='add-new-comment'>
+              <input placeholder='Add new comment' autoFocus onChange={newCommentInputHandler} />
+              {!isButtonDisable && <button onClick={addNewCommentHandler}>+</button>}
+            </div>
             :
             <Button onClick={() => history.push('/auth')}>
               Login
@@ -70,7 +94,7 @@ const CommentsList = (props) => {
           }
           {
             commentsCount === 0 ?
-            <p>No comments now</p>
+            <h6 className='no-comments-header'>No comments now</h6>
             :
             <ul>
               {commentsData.map(comment=><PlaceComentItem key={comment.id} comment={comment}/>)}
